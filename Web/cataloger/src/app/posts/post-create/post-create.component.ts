e5fcb3e862d2334a311e1794b4f81abcc2266322
Will <willengel4@gmail.com>
2018-12-25 19:19:26 -0500

    form validation

--- Web/cataloger/src/app/posts/post-create/post-create.component.ts
+++ Web/cataloger/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -8,12 +10,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost(postInput: HTMLTextAreaElement) {
-    const post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }

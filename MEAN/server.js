8cd310d7130e4c6e2345994b12da2a04a1a29f0a
Will <willengel4@gmail.com>
2018-12-27 16:36:37 -0500

    using Connect

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,8 +1,24 @@
-var http = require('http');
+var connect = require('connect');
+var app = connect();
 
-http.createServer(function(req, res) {
-    res.writeHead(200, { 
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello, world!');
-}).listen(3000);
+var logger = function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
+var helloWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
+
+var goodbyeWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+};
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000/');

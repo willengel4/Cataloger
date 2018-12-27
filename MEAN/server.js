df22660ac8fe1b2d3278a67cb6696f207ab67c23
Will <willengel4@gmail.com>
2018-12-27 17:52:04 -0500

    setup express

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,24 +1,8 @@
-var connect = require('connect');
-var app = connect();
-
-var logger = function(req, res, next) {
-    console.log(req.method, req.url);
-    next();
-};
-
-var helloWorld = function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-};
-
-var goodbyeWorld = function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Goodbye World');
-};
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
+var express = require('express');
+var app = express();
+    app.use('/', function(req, res) {
+    res.send('Hello World');
+});
 app.listen(3000);
-
 console.log('Server running at http://localhost:3000/');
+module.exports = app;
